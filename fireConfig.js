6fd065ba982f6b6602bd0a858ead8f296f34237a
woobearr <144994508+woobearr@users.noreply.github.com>
2024-06-01 23:37:13 +0800

    Add files via upload

---
+++ fireConfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyAu7E9JCPOz77u54ext64r2VfZT2vol2VQ",
+  authDomain: "crud-f7474.firebaseapp.com",
+  databaseURL: "https://crud-f7474-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "crud-f7474",
+  storageBucket: "crud-f7474.appspot.com",
+  messagingSenderId: "599810666281",
+  appId: "1:599810666281:web:060821239e3ad67ceed138"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
---
+++ myscript.js
@@ -0,0 +1,94 @@
+var rollV, nameV, genderV, addressV, emailV;
+
+function readFom() {
+  rollV = document.getElementById("roll").value;
+  nameV = document.getElementById("name").value;
+  genderV = document.getElementById("gender").value;
+  addressV = document.getElementById("address").value;
+  emailV = document.getElementById("email").value;
+  surname1 = document.getElementById("surname").value;
+  middlename1 = document.getElementById("middlename").value;
+  Swal.fire("Data Read Succesfully!");
+  console.log(rollV, nameV, addressV, genderV);
+}
+
+document.getElementById("insert").onclick = function () {
+  readFom();
+
+  firebase
+    .database()
+    .ref("student/" + rollV)
+    .set({
+      rollNo: rollV,
+      name: nameV,
+      gender: genderV,
+      address: addressV,
+      email: emailV,
+      surname: surname1,
+      middlename: middlename1
+    });
+    Swal.fire("Data Inserted Succesfully!");
+  document.getElementById("roll").value = "";
+  document.getElementById("name").value = "";
+  document.getElementById("gender").value = "";
+  document.getElementById("address").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("surname").value = "";
+  document.getElementById("middlename").value = "";
+};
+
+document.getElementById("read").onclick = function () {
+  readFom();
+
+  firebase
+    .database()
+    .ref("student/" + rollV)
+    .on("value", function (snap) {
+      document.getElementById("roll").value = snap.val().rollNo;
+      document.getElementById("name").value = snap.val().name;
+      document.getElementById("gender").value = snap.val().gender;
+      document.getElementById("address").value = snap.val().address;
+      document.getElementById("email").value = snap.val().email;
+      document.getElementById("surname").value = snap.val().surname;
+      document.getElementById("middlename").value = snap.val().middlename;
+    });
+};
+
+document.getElementById("update").onclick = function () {
+  readFom();
+
+  firebase
+    .database()
+    .ref("student/" + rollV)
+    .update({
+      //   rollNo: rollV,
+      name: nameV,
+      gender: genderV,
+      address: addressV,
+      email: emailV
+    });
+    Swal.fire("Data Updated Succesfully!");
+  document.getElementById("roll").value = "";
+  document.getElementById("name").value = "";
+  document.getElementById("gender").value = "";
+  document.getElementById("address").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("surname").value = "";
+  document.getElementById("middlename").value = "";
+};
+document.getElementById("delete").onclick = function () {
+  readFom();
+
+  firebase
+    .database()
+    .ref("student/" + rollV)
+    .remove();
+    Swal.fire("Data Deleted Succesfully!");
+  document.getElementById("roll").value = "";
+  document.getElementById("name").value = "";
+  document.getElementById("gender").value = "";
+  document.getElementById("address").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("surname").value = "";
+  document.getElementById("middlename").value = "";
+};
